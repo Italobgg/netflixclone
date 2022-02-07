6d159f4d567927982d1f3d34087d5bdf9fc43f53
Italo Bartolomeu <italobartolomeugarcia@gmail.com>
2022-02-07 14:17:52 -0300

    Featuresdsjs, css e estilizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState} from 'react';
+import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setfeaturedData] = useState(null);
 
   useEffect(()=>{
     const loadAll = async () => {
       // pegando lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      console.log(chosenInfo);
+      setfeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -19,6 +30,14 @@ export default () => {
 
     return (
       <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+
+
+
         <section className="lists">
           {movieList.map((item, key)=>(
             <MovieRow key={key} title={item.title} items={item.items} />
